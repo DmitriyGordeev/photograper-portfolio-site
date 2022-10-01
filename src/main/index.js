1f5beb9b2feed71fc857bd85f4e5131475fe6a7b
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-10-01 12:05:57 +0300

    resolving image import'

--- src/main/index.js
+++ src/main/index.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 
 // import App from "./App";
 import './index.css'
+import portraitImage from './../../resources/images/rafaella-mendes-diniz-et_78QkMMQs-unsplash.jpg';
 
 
 const initialState = {};
@@ -27,7 +28,7 @@ const root = document.getElementById('app');
 ReactDOM.render(
     <Provider store={store}>
         <div>
-            {"DIV"}
+            <img src={portraitImage} alt={"portrait"} className={"aspect-ratio"} />
         </div>
     </Provider>,
 root);
--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,15 @@ const config = {
                     { loader: "style-loader" },
                     { loader: "css-loader" }
                 ]
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: 'file-loader',
+                exclude: /node_modules/,
+                options: {
+                    name: '/resources/images/[name].[ext]'
+                }
             }
         ],
     }
