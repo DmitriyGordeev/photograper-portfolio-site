7c019f971d5f340930fd38a6cc314a030253ee53
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-10-05 15:11:57 +0300

    saving menu item behavior

--- src/main/Menu.js
+++ src/main/Menu.js
@@ -12,27 +12,46 @@ class Menu extends React.Component {
     render() {
         return (
             <div className={"top-menu-holder"}>
-                <div className={"menu-item"}>
-                    <p>
-                        <a href={"https://google.com"}>
-                            <span>ABOUT ME</span>
-                        </a>
-                    </p>
-                </div>
-                <div className={"menu-item"}>
-                    <p>
-                        <a href={"https://google.com"}>
-                            <span>SOCIALS</span>
-                        </a>
-                    </p>
-                </div>
-                <div className={"menu-item"}>
-                    <p>
-                        <a href={"https://google.com"}>
-                            <span>CONTACT</span>
-                        </a>
-                    </p>
-                </div>
+                <ul>
+                    <li className={"menu-item"}>
+                        <div className={"hover-underlay"}></div>
+                        <a href={"https://google.com"}><span>ABOUT ME</span></a>
+                    </li>
+                    <li className={"menu-item"}>
+                        <div className={"hover-underlay"}>
+                            <div></div>
+                        </div>
+                        <a href={"https://google.com"}><span>SOCIALS</span></a>
+                    </li>
+                    <li className={"menu-item"}>
+                        <div className={"hover-underlay"}>
+                            <div></div>
+                        </div>
+                        <a href={"https://google.com"}><span>CONTACT</span></a>
+                    </li>
+                </ul>
+
+                {/*<div className={"menu-item"}>*/}
+                {/*    <p>*/}
+                {/*        <a href={"https://google.com"}>*/}
+                {/*            <span>ABOUT ME</span>*/}
+                {/*        </a>*/}
+                {/*    </p>*/}
+                {/*</div>*/}
+                {/*<div className={"menu-item"}>*/}
+                {/*    <p>*/}
+                {/*        <a href={"https://google.com"}>*/}
+                {/*            <span>SOCIALS</span>*/}
+                {/*        </a>*/}
+                {/*    </p>*/}
+                {/*</div>*/}
+                {/*<div className={"menu-item"}>*/}
+                {/*    <p>*/}
+                {/*        <a href={"https://google.com"}>*/}
+                {/*            <span>CONTACT</span>*/}
+                {/*        </a>*/}
+                {/*    </p>*/}
+                {/*</div>*/}
             </div>
         )
     }
