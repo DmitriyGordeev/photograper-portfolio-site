b0e655dbb59c814ca83d1ab26c1b8085ed966c85
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-10-14 20:35:48 +0300

    saving colors

--- src/main/palette.js
+++ src/main/palette.js
@@ -1,8 +1,9 @@
 const colors = {
     dark: "#0a0a1a",
+    light: "#eee5e9",
     blue: "#1E4BA2",
-    light: "#eee5e9"
-
+    lightBlue: "#92DCE5",
+    red: "#d64933"
 }
 
 export default colors;
