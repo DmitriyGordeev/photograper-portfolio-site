6ddcd0e0f5b6f5771fa1298d358b77861ddee324
Dimas <dimaster@email.com>
2023-03-26 15:15:07 +0300

    exp/ edits

--- send_email.php
+++ send_email.php
@@ -1,13 +1,11 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        ini_set("SMTP", "smtp.f0732619.xsph.ru");
-        ini_set("sendmail_from", "f0732619@f0732619.xsph.ru");
+        ini_set("SMTP", "");
+        ini_set("sendmail_from", "");
 
-        $message = "The mail message was sent with the following mail setting:\r\nSMTP = smtp.f0732619.xsph.ru\r\nsmtp_port = 25\r\nsendmail_from = f0732619@f0732619.xsph.ru";
-
-        $headers = "From: f0732619@f0732619.xsph.ru";
-
-        mail("gordeev.dmitriy@physics.msu.ru", "Testing", $message, $headers);
-        echo "Check your email now....&lt;BR/>";
+        # TODO: fill this
+        $message = "";
+        $headers = "From: website.ru";
+        mail("email@physics.msu.ru", "Testing", $message, $headers);
     }
 ?>
--- src/exp/App.js
+++ src/exp/App.js
@@ -2,33 +2,6 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import "./App.css";
 
-import portraitExample from './../../resources/images/portrait.jpg';
-import portraitExample2 from './../../resources/images/portrait2.jpg';
-import room from "../../resources/images/room.jpg";
-import reflection from "../../resources/images/camera_reflection.png";
-import img1 from "../../resources/images/img1.jpg";
-import img2 from "../../resources/images/img2.jpg";
-import img3 from "../../resources/images/img3.jpg";
-
-
-// import {TextData} from "../main/TextData";
-// let td = new TextData();
-// td.foo();
-
-
-
-import AsyncImage from "../main/AsyncImage";
-
-const images = [
-    portraitExample,
-    portraitExample2,
-    room,
-    reflection,
-    img1,
-    img2,
-    img3
-]
-
 
 class App extends React.Component {
     constructor(props) {
@@ -37,69 +10,9 @@ class App extends React.Component {
         }
     }
 
-
-    promiseExample() {
-        // const promise1 = new Promise((resolve, reject) => {
-        //     setTimeout(() => {
-        //         resolve('foo');
-        //     }, 300);
-        // });
-
-        // const promise = new Promise(res => {
-        //     setTimeout(() => {
-        //         res('foo');
-        //     }, 600);
-        // });
-
-        const promise = new Promise((res) => {
-            setTimeout(() => {
-                res("res arg");
-                console.log("timeout callback");
-            }, 10000);
-        });
-        return promise;
-    }
-
-
-    async foo() {
-        // заходит сюда - и засыпает, возвращая выполнение в функцию render()
-        let p = await this.promiseExample();
-
-        for (let i = 0; i < 10000; i++) {
-            for(let j = 0; j < 70000; j++) {
-                let x = i + j;
-            }
-        }
-
-        console.log("foo end");
-        return p;
-    }
-
-
     render() {
-
-        let out = this.foo();
-
-        // почему этот лог выполняется в последнюю очередь
-        out.then((resValue) => { console.log("out.then resValue = " + resValue) });
-
-
-        // let p = this.promiseExample();
-        // // p.then(() => {
-        // //     // это как раз наша функция res внутри promise
-        // //     console.log("promise 'then' callback");
-        // // });
-
-        console.log("render after");
-
-
         return (
-            <div className={"main"} style={{
-                width: "100%",
-                height: "100%"
-            }}>
-                <AsyncImage size={300} src={img2} hfix={true} />
-            </div>
+            <div/>
         );
     }
 }
